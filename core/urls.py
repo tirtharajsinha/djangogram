0e32ac237eedc7fa7be0832c76584088c71772eb
Tirtharaj Sinha <sinhatirtharaj@gmail.com>
2024-03-25 21:43:13 +0530

    added home.urls in core/urls

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('chat/', include('chat.urls')),
-    path('admin/', admin.site.urls),
+    path("", include("home.urls")),
+    path("chat/", include("chat.urls")),
+    path("admin/", admin.site.urls),
 ]
